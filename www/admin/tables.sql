a71df4905eb26502869ccacaea3dc1741cb87397
freejoe76 <jmurphy@denverpost.com>
2017-09-27 10:37:56 -0400

    DB table improvements

--- www/admin/tables.sql
+++ www/admin/tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `guesses` (
+CREATE TABLE `mapgame_guesses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `games_id` int(11) NOT NULL,
   `guess` float NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `guesses` (
   KEY `games_id` (`games_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
-CREATE TABLE `games` (
+CREATE TABLE `mapgame_games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `slug` varchar(100) COLLATE utf8_bin NOT NULL,
   `title` varchar(250) COLLATE utf8_bin NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `games` (
   `guess_average` float NOT NULL DEFAULT '0',
   `wrong_guess_average` float NOT NULL DEFAULT '0',
   `guesses` int(11) NOT NULL DEFAULT '0',
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `correct` int(11) NOT NULL DEFAULT '0',
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `slug` (`slug`)
@@ -28,20 +28,20 @@ CREATE TABLE `games` (
 
 
 # Should we want to track guesses / games over time
-CREATE TABLE `count` (
+CREATE TABLE `mapgame_count` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `games` int(11) DEFAULT '0' NOT NULL,
   `guesses` int(11) DEFAULT '0' NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
 # To give more insight into the group games
-CREATE TABLE `group_guesses` (
+CREATE TABLE `mapgame_group_guesses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `games_id` int(11) NOT NULL,
   `correct` float NOT NULL,
   `ip` varchar(30) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `games_id` (`games_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=64391 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
